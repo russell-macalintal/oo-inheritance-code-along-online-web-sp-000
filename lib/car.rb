23435db9fda1fd16608243b7bdc8a6f1d1cf540e
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-28 01:40:11 +0000

    Automatically backed up by Learn

--- lib/car.rb
+++ lib/car.rb
@@ -2,10 +2,8 @@ require_relative "./vehicle.rb"
 
 class Car < Vehicle
 
-  class Car < Vehicle
-    def go
-      "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
-    end
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
   end
 
 end
